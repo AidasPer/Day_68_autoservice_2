114b4f574cd7c322d7c6d9d1314a2249a96d4e75
perai0980@gmail.com <perai0980@gmail.com>
2025-08-19 21:21:17 +0300

    autoservice 2 complete

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Car, Service, Order
+
+admin.site.register(Car)
+admin.site.register(Service)
+admin.site.register(Order)
