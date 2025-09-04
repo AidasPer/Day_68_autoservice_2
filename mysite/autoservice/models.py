ddce739101d264d77c221efa891287b76aa40084
perai0980@gmail.com <perai0980@gmail.com>
2025-09-04 21:13:54 +0300

    10: sutvarkyti nuotraukos matmenys kad butu apvali nuotrauka

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -2,14 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser, UserManager
 from django.utils import timezone
 from tinymce.models import HTMLField
-# Create your models here.
+from PIL import Image
 
 
 class CustomUser(AbstractUser):
     photo = models.ImageField(verbose_name="Photo", upload_to="profile_pics" , null=True, blank=True)
     location = models.TextField(verbose_name="Location", null=True, blank=True)
 
-    objects = UserManager()
+    def save(self, *, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+        if self.photo:
+            img = Image.open(self.photo.path)
+            min_side = min(img.width, img.height)
+            left = (img.width - min_side) // 2
+            top = (img.height - min_side) // 2
+            right = left + min_side
+            bottom = top + min_side
+            img = img.crop((left, top, right, bottom))
+            img = img.resize((300, 300), Image.LANCZOS)
+            img.save(self.photo.path)
+    # objects = UserManager()
 
 
 class Car(models.Model):
